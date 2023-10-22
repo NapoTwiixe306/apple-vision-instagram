388215aa40ad048cbbb9d04de28a4bbfe41046a4
julien <julien.milants@gmail.com>
2023-10-22 13:59:41 +0200

    via terminal

--- src/Views/HomePageExtends.jsx
+++ src/Views/HomePageExtends.jsx
@@ -2,7 +2,7 @@ function HomePageExtends(){
     return(
         <>
             <div className="HomePageExtends">
-                <h1>Hello World</h1>
+                <h1>Hello </h1>
             </div>
         </>
     )
