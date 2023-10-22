9909de7937a05d68ab2ef7620f57fafe56d9347a
julien <julien.milants@gmail.com>
2023-10-22 12:24:41 +0200

    start Legend

--- src/Views/HomePages.jsx
+++ src/Views/HomePages.jsx
@@ -21,7 +21,6 @@ function HomePages() {
                         <div className="Home__Suggestion">
                             <div className="Suggestion__Content">
                                 <p>Suggestion</p>
-
                             </div>
                         </div>
                     </div>
