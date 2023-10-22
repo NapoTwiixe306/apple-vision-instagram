b419d0154575bbba05ce471f57b5976f5f18284d
julien <julien.milants@gmail.com>
2023-10-22 13:41:41 +0200

    suite de Publication

--- src/Views/HomePages.jsx
+++ src/Views/HomePages.jsx
@@ -1,6 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faComment, faBookmark } from '@fortawesome/free-solid-svg-icons';
 import Sidebar from "../Widget/Sidebar";
 import Navbar from "../Widget/Navbar";
-
+import Warren from '../img/Legend/Warren.png'
 function HomePages() {
     return (
         <>
@@ -8,14 +10,30 @@ function HomePages() {
                 <div className="Home">
                     <div className="Content">
                         <div className="Home__Posts">
-                            <div className="Publication">
-                                <p>Publication</p>
+                            <div className="Posts">
+                                <div className="Publication">
+                                    <p>Publication</p>
+                                </div>
+                                <div className="Stat">
+                                    <p>
+                                        <strong>
+                                            <FontAwesomeIcon icon={faHeart} style={{ color: 'red' }} />{' '}
+                                            20 004 Likes -{' '}
+                                            <FontAwesomeIcon icon={faComment} style={{ color: 'white' }}/>{' '}
+                                            5889 Commentaires -{' '}
+                                            <FontAwesomeIcon icon={faBookmark} style={{ color: 'white' }}/>{' '}
+                                            527 Signet
+                                        </strong>
+                                    </p>
+                                </div>
                             </div>
                             <div className="Legend">
-                                {/*img*/}
-                                <p><strong>Warren Buffet</strong></p>
-                                <p><strong>0 Following - 1.6M Followers</strong></p>
-                                <p><strong>10 Posts</strong></p>
+                                <img src={Warren} alt=""/>
+                                <div className="txt">
+                                    <p><strong>Warren Buffet</strong></p>
+                                    <p><strong>0 Following - 1.6M Followers</strong></p>
+                                    <p><strong>10 Posts</strong></p>
+                                </div>
                             </div>
                         </div>
                         <div className="Home__Suggestion">
